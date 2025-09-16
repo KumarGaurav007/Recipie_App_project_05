9ce4027e8b23768e50a620ff0987a2e5922184d3
KumarGaurav007 <gauravkumarsk07@gmail.com>
2025-09-16 22:06:10 +0530

    Nav bar code updated

---
+++ Food App/src/Components/HeroSection.jsx
@@ -0,0 +1,14 @@
+export default function HeroSection(){
+    return (
+        <div className="section">
+            <div className="col">
+                <h1 className="title">What Are We About</h1>
+                <p className="info">FoodiesHub is a place where you can please your sole and tummy with delicious food recipies of all Cuisine. And our service is absolutely free. So start exploring now.</p>
+                <button className="btn">Explore Now</button>
+            </div>
+            <div className="col">
+                
+            </div>
+        </div>
+    )
+}
